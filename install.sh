6becc74210e47cc4cef66b4cdab8b4d696a19768
Momir Milutinović <milutinm462@gmail.com>
2025-05-23 16:24:47 +0200

    feat: Install Vundle

--- install.sh
+++ install.sh
@@ -53,9 +53,8 @@ ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zsh/aliases ~/.aliases
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 
-# Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
 # Change shell to zsh
